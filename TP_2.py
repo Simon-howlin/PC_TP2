b7b333106248401537336ae5cec3068e8c276bcc
Simon Howlin <showlin@udesa.edu.ar>
2024-05-08 01:14:56 -0300

    poner la secuencia en la imagen

--- TP_2.py
+++ TP_2.py
@@ -1,3 +1,5 @@
+from PIL import Image
+import numpy as np
 """
 Bloques esenciales:
 -Convertidor Char a Num --> LISTO
@@ -5,7 +7,7 @@ Bloques esenciales:
 -Aplicador Kuwahara
 -Encodeador de mensajes ocultos
     - Transformar mensaje a secuencia --> LISTO
-    - Poner secuencia en una imagen
+    - Poner secuencia en una imagen --> CASI LISTO
 -Desencodeador de mensajes ocultos
     - Sacar secuencia de una imagen
     - Transformar secuencia a mensaje --> LISTO
@@ -62,8 +64,43 @@ def seq_a_string(seq):
         else:
             guardar_char += str(n - 1)
 
-
-
-
 #seq = [9, -1, 2, 6, -1, 2, 3, -1, 2, -1, 3, 10, -1, 3, 8, -1, 4, 3, -1, 4, -1, 5, 5, -1, 2, 4, -1, 2, 6, -1, 3, 8, -1, 6, -1, 2, 10, -1, 3, 1, -1, 5, 2, -1, 2, 10, -1, 4, 1, -1, 0]
 #print(seq_a_string(seq))
+
+def seq_en_imagen():
+    im = Image.open("imagen.jpeg")
+    width,height = im.size
+    array = np.array(im)
+    # array[num_fila,num_col,CANAL] # NUM_CANAL: rojo = 0, verde = 1, azul = 2
+
+    secuencia = string_a_seq()
+
+    for i in range(0,height,2):
+        for j in range(0,width,2):
+            varianzas = []
+            contador = 0
+            can_sup_izq = []
+            can_sup_der = []
+            can_inf_izq = []
+            can_inf_der = []
+            for canal in (0,3):
+                sup_izq = array[i,j,canal]
+                can_sup_izq.append(sup_izq[canal])
+                sup_der = array[i,j+1,canal]
+                can_sup_der.append(sup_der[canal])
+                inf_izq = array[i+1,j,canal]
+                can_inf_izq.append(inf_izq[canal])
+                inf_der = array[i+1,j+1,canal]
+                can_inf_der.append(inf_der[canal])
+
+                chek_vari = np.var[sup_der[canal],sup_izq[canal], inf_izq[canal]]
+                chek_vari.append(varianzas)
+
+            min_varianzas = min(varianzas)
+            modificar_x_canal = varianzas.index(min_varianzas)
+            promedio_menor_var = sum(can_sup_izq[modificar_x_canal], can_sup_der[modificar_x_canal], can_inf_izq[modificar_x_canal], can_inf_der[modificar_x_canal]) / 3
+            remplazar_canal = promedio_menor_var + secuencia[contador]
+            
+            canal_modificado = inf_der[modificar_x_canal] = remplazar_canal
+            
+            contador += 1
